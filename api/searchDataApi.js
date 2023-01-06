94d19c6f23547aeb73f2740689b2675a8c0ba4f5
Md Mizanur Rahman <118674977+webmmr@users.noreply.github.com>
2023-01-06 12:19:20 +0600

    Update searchDataApi.js

--- api/searchDataApi.js
+++ api/searchDataApi.js
@@ -1,1 +1,19 @@
-js
+import axios from "axios";
+import CurrentWeather from "../components/CurrentWeather";
+
+const searchDataApi = (city) => {
+  return axios
+    .get("https://api.weatherapi.com/v1/search.json", {
+      params: {
+        key: "77f64c2745ca46eca0962940222712",
+        q: city,
+      },
+    })
+    .then(({ data }) => {
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+    })
+    .catch((e) => console.log(e));
+};
+
+export default searchDataApi;
